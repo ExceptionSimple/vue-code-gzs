1d557bb1f76dce58b1d428cb0d700ebe61a3d0f9
XiaoLing <lingdu117@gmail.com>
2025-10-13 11:31:39 +0800

    feat: 初步 Activities

--- src/views/Activities/Activities.vue
+++ src/views/Activities/Activities.vue
@@ -2,8 +2,45 @@
 </script>
 
 <template>
-  activities
+  <div class="header">
+    <div class="icon">
+      <span class="iconfont">&#xe600;</span>
+    </div>
+    <div class="title">活动中心</div>
+    <div class="sub-title">参与丰富多彩的技术活动，提升编程技能，结识志同道合的伙伴</div>
+  </div>
 </template>
 
 <style scoped>
+.header {
+  text-align: center;
+  padding: 80px 0;
+}
+.header .icon {
+  width: 50px;
+  height: 50px;
+  margin: 0 auto;
+  text-align: center;
+  line-height: 50px;
+  color: white;
+  border-radius: 10px;
+  padding: 10px;
+  background-image: var(--gradient-primary);
+}
+.header .iconfont {
+  font-size: 2.5rem;
+}
+.header .title {
+  font-weight: 700;
+  font-size: 3rem;
+  background-image: var(--gradient-primary);
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
+  margin: 15px 0;
+}
+.header .sub-title {
+  color: var(--normal-text);
+  font-size: 1.3rem;
+}
 </style>
---
+++ src/views/Activities/components/ComingSoon.vue
@@ -0,0 +1,25 @@
+<script setup>
+</script>
+
+<template>
+  <div class="coming-soon">
+    <div class="header">
+      <h2> <span class="iconfont">&#xe600;</span> 即将开始</h2>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  display: flex;
+  justify-content: space-between;
+  vertical-align: middle;
+}
+.header .el-button {
+  background-color: transparent;
+}
+.header .iconfont {
+  font-size: 20px;
+  color: var(--theme-color);
+}
+</style>
