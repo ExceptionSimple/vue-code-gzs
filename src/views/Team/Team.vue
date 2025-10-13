fd0652563230d2cfd1d069e123dbd6f9d1454a1c
XiaoLing <lingdu117@gmail.com>
2025-10-13 10:15:59 +0800

    feat: Team Header

--- src/views/Team/Team.vue
+++ src/views/Team/Team.vue
@@ -2,8 +2,45 @@
 </script>
 
 <template>
-  team...
+  <div class="header">
+    <div class="icon">
+      <span class="iconfont">&#xe600;</span>
+    </div>
+    <div class="title">我们的团队</div>
+    <div class="sub-title">由经验丰富的导师和充满激情的学生组成的技术团队</div>
+  </div>
 </template>
 
 <style scoped>
+.header {
+  text-align: center;
+  padding: 80px 0;
+}
+.header .icon {
+  width: 50px;
+  height: 50px;
+  margin: 0 auto;
+  text-align: center;
+  line-height: 50px;
+  color: white;
+  border-radius: 10px;
+  padding: 10px;
+  background-image: var(--gradient-primary);
+}
+.header .iconfont {
+  font-size: 2.5rem;
+}
+.header .title {
+  font-weight: 700;
+  font-size: 3rem;
+  background-image: var(--gradient-primary);
+  -webkit-background-clip: text;
+  background-clip: text;
+  color: transparent;
+  margin: 15px 0;
+}
+.header .sub-title {
+  color: var(--normal-text);
+  font-size: 1.3rem;
+}
 </style>
