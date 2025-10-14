01db6987f51b4835b34599d811375fa44eaf25ed
XiaoLing <lingdu117@gmail.com>
2025-10-14 11:23:56 +0800

    update: 打字效果 style

--- src/views/Index/components/BigScreenContent.vue
+++ src/views/Index/components/BigScreenContent.vue
@@ -1,12 +1,48 @@
-<script setup>
+<script lang="ts" setup>
+import { TypedComponent, type TypedOptions } from "typed-vue3";
+
+const options1: TypedOptions = {
+  strings: ["智慧代码工作室"],
+  typeSpeed: 80,
+  // loop: true,
+  onComplete(self) {
+    const titleCursor = document.querySelector('.type1 .typed-cursor');
+    if (titleCursor) {
+      (titleCursor as HTMLElement).style.opacity = '0';
+    }
+  }
+};
+const options2: TypedOptions = {
+  strings: ["在这里，用代码改变世界，与志同道合的伙伴一起成长"],
+  typeSpeed: 60,
+  // loop: true,
+  onComplete(self) {
+    const titleCursor = document.querySelector('.type2 .typed-cursor');
+    if (titleCursor) {
+      (titleCursor as HTMLElement).style.opacity = '0';
+    }
+  }
+};
+
 </script>
 
 <template>
+
   <div class="big-screen">
-    <div class="container">
-      <h1>智慧代码工作室</h1>
+    <!-- <div class="container">
+      <h1></h1>
       <div class="sub-title">在这里，用代码改变世界，与志同道合的伙伴一起成长</div>
-    </div>
+    </div> -->
+    <TypedComponent :options="options1" class="type1">
+      <!-- <span class="typing" /> -->
+      <h1 class="typing"></h1>
+      <!-- <div class="sub-title"></div> -->
+    </TypedComponent>
+    <TypedComponent :options="options2" class="type2">
+      <!-- <span class="typing" /> -->
+      <!-- <h1 class="typing"></h1> -->
+      <div class="sub-title typing"></div>
+    </TypedComponent>
   </div>
 </template>
 
@@ -22,11 +58,23 @@
   align-items: center;
 }
 .big-screen h1 {
+  display: inline-block;
   font-size: .8rem;
   color: var(--theme-color);
 }
 .big-screen .sub-title {
+  margin-top: 15px;
+  display: inline-block;
   font-size: .3rem;
   color: var(--normal-text);
+}
+</style>
+
+<style>
+.big-screen .type1 .typed-cursor {
+  font-size: .8rem;
+}
+.big-screen .type2 .typed-cursor {
+  font-size: .3rem;
 }
 </style>
